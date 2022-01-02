1df1347afa6effc1024935b6ab1b5c0185484a02
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-02 17:20:35 +0700

    Update ContentFooter.vue

--- src/pages/Layout/ContentFooter.vue
+++ src/pages/Layout/ContentFooter.vue
@@ -1,36 +1,14 @@
 <template>
-  <footer class="footer">
-    <div class="container">
-      <!-- <nav>
-        <ul>
-          <li>
-            <a href="https://www.creative-tim.com">Creative Tim</a>
-          </li>
-          <li>
-            <a href="https://creative-tim.com/presentation">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a href="http://blog.creative-tim.com">
-              Blog
-            </a>
-          </li>
-          <li>
-            <a href="https://www.creative-tim.com/license">
-              Licenses
-            </a>
-          </li>
-        </ul>
-      </nav> -->
-      <div class="copyright text-center">
+    <footer class="footer">
+        <div class="container">
+            <!-- <div class="copyright text-center">
         &copy; {{ new Date().getFullYear() }}
         <a href="#" target="_blank">Experience Consultant and Solution</a>
-      </div>
-    </div>
-  </footer>
+      </div> -->
+        </div>
+    </footer>
 </template>
 <script>
-export default {};
+export default {}
 </script>
 <style></style>
