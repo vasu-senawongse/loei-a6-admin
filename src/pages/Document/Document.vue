a3298a372e324e798a785d1905f51a3b95df792d
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-02 17:36:11 +0700

    Update Document.vue

--- src/pages/Document/Document.vue
+++ src/pages/Document/Document.vue
@@ -2,6 +2,10 @@
     <div class="content">
         เพิ่มเติมเอกสารที่เกี่ยวข้องได้ที่
         <a href="http://pubhtml5.com/" target="_blank">http://pubhtml5.com/</a>
+        <p>
+            เข้าสู่ระบบด้วย ผู้ใช้งานอีเมล : loei.datas@gmail.com รหัสผ่าน :
+            12345Loei
+        </p>
     </div>
 </template>
 
