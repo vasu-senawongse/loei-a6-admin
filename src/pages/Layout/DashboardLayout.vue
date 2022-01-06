d88807dcf6910a51937b330a598302df4c8193d4
Vasu Senawongse <46882733+vasu-senawongse@users.noreply.github.com>
2022-01-06 08:56:40 +0700

    Update DashboardLayout.vue

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -54,12 +54,6 @@
         <div class="main-panel">
             <top-navbar></top-navbar>
 
-            <fixed-plugin
-                :color.sync="sidebarBackground"
-                :image.sync="sidebarBackgroundImage"
-            >
-            </fixed-plugin>
-
             <dashboard-content> </dashboard-content>
 
             <content-footer v-if="!$route.meta.hideFooter"></content-footer>
@@ -72,7 +66,6 @@ import TopNavbar from './TopNavbar.vue'
 import ContentFooter from './ContentFooter.vue'
 import DashboardContent from './Content.vue'
 import MobileMenu from '@/pages/Layout/MobileMenu.vue'
-import FixedPlugin from './Extra/FixedPlugin.vue'
 
 export default {
     components: {
@@ -80,7 +73,6 @@ export default {
         DashboardContent,
         ContentFooter,
         MobileMenu,
-        FixedPlugin,
     },
     data() {
         return {
